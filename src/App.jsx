42240514413ffde540d3803e6e2c5379925c9792
KINGSLEY DURUCHUKWU <142257235+DURUCODES@users.noreply.github.com>
2025-08-27 00:49:34 +0100

    password page modified

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import Homepage from "./componets/Homepage";
 import ConnectPage from "./componets/ConnectPage";
-import Passwordpage from "./componets/passwordpage";
+import Adminpage from "./componets/Passwordpage";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Homepage />} />
       <Route path="/connect" element={<ConnectPage />} />
-      <Route path="/admin" element={<Passwordpage />} />
+      <Route path="/admin" element={<Adminpage />} />
     </Routes>
   );
 }
